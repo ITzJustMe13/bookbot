4cfc2787436e9b308dbe797a5e8ae6bf9203e583
ITzJustMe13 <oreosmilesoo@gmail.com>
2024-06-23 17:38:26 +0100

    added count_words

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("hello world")
+def count_words(content):
+    words = content.split()
+    print(len(words))
+
+def main():
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+    
+    count_words(file_contents)
+
+
+
+main()
